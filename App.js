cad3a9654a7d041271b89987afe91b981bfaa7e0
AnnaJ <annaspaulimp@gmail.com>
2024-06-13 00:08:50 +0530

    updated

--- App.js
+++ App.js
@@ -10,13 +10,28 @@ app.use(express.json())
 app.post("/add",
     (req, res) => {
         let input = req.body
-        let faculty=new facultymodel(input)
+        let faculty = new facultymodel(input)
         faculty.save()
         console.log(faculty)
-        res.json({"status":"success"})
+        res.json({ "status": "success" })
     }
 )
 
+
+app.post(
+    "/search", (req, res) => {
+        let input = req.body
+        facultymodel.find(input).then(
+            (data) => {
+                res.json(data)
+            }
+        ).catch(
+            (error) => {
+                res.json(error)
+            }
+        )
+    }
+)
 
 app.listen(8089, () => {
     console.log("server started")
